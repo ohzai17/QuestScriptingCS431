2d8fb680e8b39da6f99eea1729b5acc4bb6bba47
Au Sein <seina@sunypoly.edu>
2025-04-08 10:58:54 -0400

    added commands

--- quests/poknowledge/npcname.pl
+++ quests/poknowledge/npcname.pl
@@ -2,8 +2,9 @@
 # Author: [Your Name]
 # YouTube Demo: [Insert Link Here]
 
-# Zone: poknowledge
-# NPC: npcname
+# Zone Teleport: #zone poknowledge
+# NPC Spawn: #spawn 1 50 1 1 0 1000 0
+# Customize NPC: #npcname "Helper"
 
 
 sub EVENT_SAY {
